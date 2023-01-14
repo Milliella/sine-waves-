4000e64aa1e684dfe8cc5e95beeab6b95e7cc61b
Milliella <115160750+Milliella@users.noreply.github.com>
2023-01-14 13:46:01 +0000

    14/1/afternoon

---
+++ js/sketch3.js
@@ -0,0 +1,45 @@
+let y = 0;
+let x  = 0;
+let theta = 0;
+let inc = 0.05; //the amount of increment the offset each frame- speed
+let offset =100; //the amount offset from beginning of wave
+
+function setup(){
+    amplitude = windowWidth;
+    frequency = windowHeight;
+    maxAngle = (windowWidth / frequency) * TWO_PI;
+    createCanvas( windowWidth, windowHeight);
+    noStroke();
+    let length = 10;
+    //background (60, 179, 113);
+}
+
+function draw(){
+   background(255,255,255);
+    noStroke();
+    let length = 3; //amount of squares in trail, one single line, 3 differente lines ect...
+    while (theta < maxAngle + offset) {
+        amplitude = sin(theta - offset) * (windowHeight / 2); //condenses the shapes individually 
+        for (i = length; i > 1; i--) { //takes away strands
+            x = sin(theta - (i * 1)) * amplitude; //space between the seperate strands / wave pattern
+            // fill(255,- (150 / height * i), 200);
+            // ellipse(y, x + width / 2, 10);
+
+            fill(255,- (0 / height * i), 0);
+            ellipse(y, x + height / 2, 10);
+
+            fill(89,- (10 / height * i), 10);
+            ellipse(y, x + width / 40, 10);
+        }
+        theta += 0.2;
+        y = ((theta - offset) / maxAngle) * windowWidth;
+    }
+    offset += inc;
+    theta = offset;
+}
+
+
+
+
+
+
