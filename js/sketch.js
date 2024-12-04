be112840bde2e936cdac30e55b9389c0fbd64415
Milo <115160750+Milliella@users.noreply.github.com>
2024-12-04 16:17:56 +0000

    random things

--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,8 @@
 let x = 0;
 let y = 0;
 let theta = 0;
-let inc = 0.01; //the amount of increment the offset each frame
-let offset =100; //the amount offset from beginning of wave
+let inc = 1; //the amount of increment the offset each frame
+let offset =8; //the amount offset from beginning of wave
 let amplitude;
 let frequency;
 let maxAngle;
@@ -14,27 +14,30 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     noStroke();
     let length = 10;
-
 }
 
 function draw() {
     background(0);
-    noStroke();
-    let length = 10;
+    noStroke(0);
+    let length = 40;
     while (theta < maxAngle + offset) {
         amplitude = sin(theta - offset) * (windowHeight / 2);
         amplitude = ((theta - offset) / maxAngle) * (windowHeight / 2);
         for (i = length; i > 0; i--) {
             y = sin(theta - (i * 100)) * amplitude;
-            fill(90,34,34);
+            fill(10,100,34);
             square(x, y + height / 2, 20); //shape of the little trails 
             // square(x, height/2 + y, 90);
         }
         for (i = length; i > 0; i--) {
             y = sin(theta - (i * 1)) * amplitude;
-            fill(255 - (255 / length * i), 255,);
-            ellipse(x, y + height / 2, 5);
+            fill(255,0,0,);
+            circle(x, y + height / 2, 5);
+
         }
+        
+            
+        
 
         theta += 0.2;
         x = ((theta - offset) / maxAngle) * windowWidth;
